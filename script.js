f9e9717df76c012ad68d0b84b131ef130cdaa92d
Mim Hufford <mim@hotmail.co.uk>
2023-08-10 11:27:54 +0100

    Add icons and a short number format

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const shortNumberString = number => new Intl.NumberFormat("en-GB", { notation: "compact", compactDisplay: "short" }).format(number)
 
 const posts = document.getElementById('posts')
 posts.innerHTML = ''
@@ -14,9 +15,13 @@ fetch(`https://api.reddit.com/r/${subreddit}.json`)
                     <div class='post-details'>
                         <h1 class='post-title'>${post.data.title}</h1>
                         <div class='post-date'>${new Date(post.data.created_utc * 1000).toUTCString()}</div>
-                        <div class='post-comments'>${post.data.num_comments} comments</div>
-                        </div>
-                    <span class='post-score'>${post.data.score}</span>
+                    </div>
+                    <div class='post-stats'>
+                        <img class='post-comments-icon' src='comments.svg'>
+                        <div class='post-comments'>${shortNumberString(post.data.num_comments)}</div>
+                        <img class='post-score-icon' src='score.svg'>
+                        <div class='post-score'>${shortNumberString(post.data.score)}</div>
+                    </div>
                 </a>
             `
         }
